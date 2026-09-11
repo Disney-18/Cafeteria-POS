25c57d3dbeaeec24a5ee4df6bcf729c9e8473cb3
The GeekDev Studios <disneygutierrez79@gmail.com>
2026-09-11 18:40:53 -0400

    Create formatDate.js

---
+++ src/útiles/formatDate.js
@@ -0,0 +1,18 @@
+import { format } from 'date-fns';
+import { es } from 'date-fns/locale';
+
+export function formatDateTime(date) {
+  return format(new Date(date), "dd/MM/yyyy HH:mm", { locale: es });
+}
+
+export function formatTime(date) {
+  return format(new Date(date), 'HH:mm:ss', { locale: es });
+}
+
+export function formatDateLong(date) {
+  return format(new Date(date), "EEEE d 'de' MMMM 'de' yyyy", { locale: es });
+}
+
+export function formatDayKey(date = new Date()) {
+  return format(new Date(date), 'yyyy-MM-dd');
+}
