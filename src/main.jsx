7f26a3fca3f326dc725b40e5f37e8e992c5baf98
The GeekDev Studios <disneygutierrez79@gmail.com>
2026-09-11 22:16:47 -0400

    Switch from BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import App from './App.jsx';
 import './styles/index.css';
@@ -8,7 +8,7 @@ import './styles/print.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <Toaster
         position="top-center"
@@ -21,6 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           }
         }}
       />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
